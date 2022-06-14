d9e0e9bedf70e4856d7b6d9ed256a627f8f978c8
TrofimGest <gubaoslika@gmail.com>
2022-06-14 14:51:29 +0300

    feat: complete NFTCard component

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const App = () => {
   const [loaded] = useFonts({
     InterBold: require('./assets/fonts/Inter-Bold.ttf'),
     InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
-    InterMeduim: require('./assets/fonts/Inter-Medium.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
     InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
     InterLight: require('./assets/fonts/Inter-Light.ttf'),
   });
--- components/NFTCard.js
+++ components/NFTCard.js
@@ -39,7 +39,27 @@ const NFTCard = ({ data }) => {
           padding: SIZES.font,
         }}
       >
-        <NFTTitle />
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate('Details', { data })}
+          />
+        </View>
       </View>
     </View>
   );
